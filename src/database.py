8d50cb68db71df29fbef1721e8c2209fcbf11688
Rickard Garnau <rickard.garnau89@gmail.com>
2026-02-23 14:29:44 +0100

    fix: correct syntax error in .env file

--- src/database.py
+++ src/database.py
@@ -1,4 +1,12 @@
 from psycopg_pool import ConnectionPool
-DATABASE_URL = "postgresql://postgres:benny123@localhost:5440/stock_db"
-pool = ConnectionPool(DATABASE_URL)
+from dotenv import load_dotenv
+import os
 
+load_dotenv() # reads .env from project root
+DB_PORT = os.getenv("DB_PORT")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+DATABASE_URL =f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+pool = ConnectionPool(DATABASE_URL)
