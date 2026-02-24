8339daccb1250f0a3b827fc192831fc1f55a5219
Rickard Garnau <rickard.garnau89@gmail.com>
2026-02-24 16:51:05 +0100

    feat: add Dockerfile for FastAPI container

--- src/fetcher.py
+++ src/fetcher.py
@@ -5,23 +5,26 @@ import schedule
 import time
 
 
-def fetch_data():
-    df = yf.download("KMI", start="2026-01-02", end="2026-02-24")
-    for i, row in df.iterrows():
-        stock_dict = {
-            "ticker": "KMI",
-            "price": row[("Close", "KMI")],
-            "currency": "USD",
-            "date": i.strftime("%Y-%m-%d"),
-            "volume": row[("Volume", "KMI")]
-        }
-        with pool.connection() as conn:
-            conn.execute(
-                "INSERT INTO stocks_raw (stock) VALUES (%s)",
-                (Json(stock_dict),)
-            )
+tickers = ['AAPL','ADBE','AMZN','BAC','IBM','MSFT','INTC','GOOGL','NFLX','NVDA','QCOM','WFC','BA','C','JPM','TSLA','INSM','TSM','KMI','ISRG','GEV','NBIS','SNOW','DLO','UBER','HIMS','V','PYPL','LMND','MA','AXP','MELI','ACN','DDOG','ONDS','NU','RIG','PLTR','PATH','SNAP','ORCL','BMNR','HOOD','FIG','AMD','PFE','MU','NOW','CRWV','CPNG','U','SNDK','AVGO','NKE','CRWD','CRM','RKLB','FCX','ASTS','SHOP','CPRT','DIS','GTLB','TEAM','MRVL','BE','LUMN','ANET','LUNR','NET','TXN','SBUX']
 
-schedule.every().day.at("15:31").do(fetch_data)
+def fetch_data():
+    for ticker in tickers:
+        df = yf.download(ticker, start="2026-02-23", end="2026-03-24")
+        for i, row in df.iterrows():
+            stock_dict = {
+                "ticker": ticker,
+                "price": row[("Close", ticker)],
+                "currency": "USD",
+                "date": i.strftime("%Y-%m-%d"),
+                "volume": row[("Volume", ticker)]
+        }
+            with pool.connection() as conn:
+                conn.execute(
+                    "INSERT INTO stocks_raw (stock) VALUES (%s)",
+                    (Json(stock_dict),)
+                )
+
+schedule.every().day.at("15:55").do(fetch_data)
 while True:
     schedule.run_pending()
     time.sleep(1)
