fe8d5f4243293bcc564b2fa57f5ee9cb1f14d502
Rickard Garnau <rickard.garnau89@gmail.com>
2026-02-28 20:39:02 +0100

    Add pytest tests with mocked database connection

---
+++ test_main.py
@@ -0,0 +1,34 @@
+from selectors import SelectSelector
+
+from fastapi.testclient import TestClient
+from fastapi import Depends, FastAPI
+from src.main import app, get_conn
+from src.database import pool
+from typing import Annotated
+import pytest
+
+from unittest.mock import MagicMock
+
+def override_get_conn():
+    fake_conn = MagicMock()
+    fake_conn.execute.return_value.fetchall.return_value = [{"ticker": "AAPL"}]
+    yield fake_conn
+
+app.dependency_overrides[get_conn] = override_get_conn
+
+client = TestClient(app)
+
+
+def test_get_stocks():
+    response = client.get("/stocks")
+    assert response.status_code == 200
+    assert response.json() == [{"ticker": "AAPL"}]
+
+
+
+
+
+
+def test_top_gainers():
+    response = client.get("/top_gainers")
+    assert response.status_code == 200
